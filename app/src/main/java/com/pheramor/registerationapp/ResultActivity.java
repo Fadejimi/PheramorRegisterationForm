953f48faa0c1bf279bd3accc0eed09b0a5bab88e
Adegbulugbe Fadejimi <adegbulugbefadejimi@gmail.com>
2018-08-12 01:58:07 -0500

    Fixed the result activity

--- app/src/main/java/com/pheramor/registerationapp/ResultActivity.java
+++ app/src/main/java/com/pheramor/registerationapp/ResultActivity.java
@@ -1,7 +1,11 @@
 package com.pheramor.registerationapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.pheramor.registerationapp.views.MainActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+    }
+
+    public void restart(View view) {
+        Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
